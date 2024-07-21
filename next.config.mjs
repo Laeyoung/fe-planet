0b073e348b7b983c7f03463888eef5fc0ff28f18
Laeyoung Chang <gadise@gmail.com>
2024-07-21 19:43:27 +0900

    feat: redirect to velog and google forms

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+            source: '/',
+            destination: 'https://velog.io/@laeyoung/posts',
+            permanent: true,
+        },
+        {
+          source: '/survey',
+          destination: 'https://forms.gle/N7KPEo7GSDCDF99B6',
+          permanent: true,
+        },
+      ];
+    },
+}
 
 export default nextConfig;
