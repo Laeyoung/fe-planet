e41e5a707be31163811b0e59eb18a74bc8ea03f1
Laeyoung <gadise@gmail.com>
2024-07-23 22:22:49 +0900

    Update google-sheet.ts

--- app/api/google-sheet.ts
+++ app/api/google-sheet.ts
@@ -2,9 +2,9 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 import { NextResponse } from 'next/server';
 
 export async function getRows() {
-  const doc = new GoogleSpreadsheet('스프레드시트 ID');
+  const doc = new GoogleSpreadsheet('1iErjwRDwq-ruQuLY9fNMz_xGTKlPO_fwAvD2RkrYUys');
   await doc.loadInfo(); // loads document properties and worksheets
-  const sheet = doc.sheetsByTitle['시트 이름']; 
+  const sheet = doc.sheetsByTitle['시트1']; 
   const rows = await sheet.getRows(); 
 
   const formattedRows = rows.map((row) => row._rawData); // 데이터 형식 변환 (필요에 따라 수정)
