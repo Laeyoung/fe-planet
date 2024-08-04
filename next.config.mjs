da6a2c791beba113918a13f10360237142debc70
Laeyoung <gadise@gmail.com>
2024-08-04 16:20:20 +0000

    add apis

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,11 @@
 const nextConfig = {
     async redirects() {
       return [
-        {
-            source: '/',
-            destination: 'https://velog.io/@laeyoung/%ED%95%98-%EC%9D%BC%EC%A3%BC%EC%9D%BC%EC%A7%9C%EB%A6%AC-%EC%82%AC%EC%A0%84%EA%B3%BC%EC%A0%9C-%EC%A2%80-%EA%B7%B8%EB%A7%8C',
-            permanent: true,
-        },
+        // {
+        //     source: '/',
+        //     destination: 'https://velog.io/@laeyoung/%ED%95%98-%EC%9D%BC%EC%A3%BC%EC%9D%BC%EC%A7%9C%EB%A6%AC-%EC%82%AC%EC%A0%84%EA%B3%BC%EC%A0%9C-%EC%A2%80-%EA%B7%B8%EB%A7%8C',
+        //     permanent: true,
+        // },
         {
           source: '/survey',
           destination: 'https://forms.gle/N7KPEo7GSDCDF99B6',
